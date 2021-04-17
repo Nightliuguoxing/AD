f6f0e0b5fa803ff93cab9a9b386520c0010d6650
Nightliuguoxing <2050180797@qq.com>
2021-04-17 08:59:09 +0800

    Up

--- c/Sort/SelectSort.cpp
+++ c/Sort/SelectSort.cpp
@@ -1,7 +1,42 @@
 #include "stdio.h"
 
+void swap(int a, int b){
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+void printSc(int nums[], int n){
+    int i;
+    for(i = 0; i < n; i++){
+        printf("%d", nums[i]);
+    }
+}
+
+void selectSort(int nums[], int n){
+    int i, j;
+    for(i = 0; i < n - 1; i++){
+        int min = i;
+        for(j = i + 1; j < n; j++){
+            if(nums[j] < nums[min]){
+                min = j;
+            }
+        }
+        swap(nums[min], nums[i]);
+    }
+    printSc(nums, n);
+}
+
+
 int main(){
-    
-    
-    return 0;
+    int i, n;
+    scanf("%d", &n);
+    int nums[1000000];
+    for(i = 0; i < n; i++){
+        scanf("%d", &nums[i]);
+    }
+    for(i = 0; i < n; i++){
+        printf("%d", &nums[i]);
+    }
 }
--- java/Sort/SelectSort.java
+++ java/Sort/SelectSort.java
@@ -1,28 +1,41 @@
+import java.util.*;
 public class SelectSort {
 
     public static void Select_Sort(int[] nums) {
         for (int i = 0; i < nums.length - 1; i++) {
-            int minIndex = i; // 记录最小值的下标
+            int minIndex = i; 
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[minIndex] > nums[j]) {
-                    minIndex = j; // 找出 i + 1 -> length 之间的最小值
+                    minIndex = j; 
                 }
             }
-            // 交换 当前索引 与 最小值索引
+            
             if (i != minIndex) {
                 int temp = nums[minIndex];
                 nums[minIndex] = nums[i];
                 nums[i] = temp;
             }
         }
-        // 遍历数组
+       
         for (int num : nums) {
             System.out.print(num + " ");
         }
     }
 
     public static void main(String[] args) {
-        String path = "Y:\\School\\AD\\ADA\\python\\EG.txt";
-        Select_Sort(new ReadFileUtil().ReadArrayDoc(path));
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] nums = new int[n];
+        String[] str = new String[n];
+        for(int i = 0; i < n; i++){
+            str[i] = in.next();
+            nums[i] = Integer.parseInt(str[i]);
+        }
+
+        for(int i = 0; i < n; i++){
+
+        }
+        Select_Sort(nums);
+        in.close();
     }
 }
